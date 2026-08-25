113aa2b4e5a7bae9079b5756f09ab319753ee892
Syeda1927 <syedaanih27@gmail.com>
2026-08-25 22:47:12 +0500

    Update model version in chat function

--- netlify/functions/chat.mjs
+++ netlify/functions/chat.mjs
@@ -191,7 +191,7 @@ ${languageInstruction}
                 body: JSON.stringify({
 
                     model:
-                        "llama-3.3-70b-versatile",
+                        "llama-3.1-8b-instant",
 
                     messages: [
 
